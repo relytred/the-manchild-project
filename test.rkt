07e619eea215a41ace3262ae3d7ee2aa1f689a7f
relytred <tmayle@patriotsoftware.com>
2017-03-20 15:37:43 -0400

    Test failures have error messages

--- test.rkt
+++ test.rkt
@@ -2,21 +2,22 @@
 (require racket/trace)
 
 (define expectedValues
-  '(150 -4 10 16 220 5 6 10 5 -39 true 100 false true 128 12))
+  '(150 -4 10 16 220 5 6 10 5 -39 true 10 false true 128 12))
+
+(define interpretTest
+  (lambda (i)
+    (interpret (string-append "tests/test" (string-append (number->string i) ".txt"))) ))
 
 (define runTest
   (lambda (i values)
     (cond
       ((> i 16) #t)
       ((not (list? values)) "Why")
-      ((eq? (interpret (string-append "tests/test" (string-append (number->string i) ".txt"))) (car values))
-       (runTest (+ i 1) (cdr values)))
-      (else (print (car values)) #f))))
-
-(trace runTest)
+      ((eq? (interpretTest i) (car values)) (runTest (+ i 1) (cdr values)))
+      (else (error "Test" i "should be equal to" (car values) "not" (interpretTest i)))   )))
 
 (define runTests
-  (runTest 1 expectedValues))
+  (print (runTest 1 expectedValues)))
 
 
        
