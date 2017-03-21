122c518042ebef3db11367c16347bb3ce59529d3
relytred <tmayle@patriotsoftware.com>
2017-03-21 17:06:16 -0400

    Return breaking with call/cc

--- interpret.rkt
+++ interpret.rkt
@@ -12,21 +12,20 @@ Project 1
 
 (define interpret
   (lambda (expr)
-    (runTree (parser expr) '(()()))
-    ))
+    (call/cc
+       (lambda (return)
+         (runTree (parser expr) newstate return) ))))
 
 (define block?
   (lambda (expr)
     (eq? (car expr) 'begin)))
 
 (define runTree
-  (lambda (expr state)
+  (lambda (expr state return)
     (cond
       ((null? expr) state)
-      ((not (list? state)) state)
-      ((eq? (caar expr) 'return) (returnHelp expr state))
-      ((block? (car expr)) (runTree (cdr expr) (block (cdar expr) (addSubstate state)))) 
-      (else (runTree (cdr expr) (statement (car expr) state))) )))
+      ((block? (car expr)) (runTree (cdr expr) (block (cdar expr) (addSubstate state) return) return)) 
+      (else (runTree (cdr expr) (statement (car expr) state return) return)) )))
 
 ; Helper methods to determine which element is an operator or an operand in the statemnt
 
@@ -35,21 +34,21 @@ Project 1
 (define operand2 caddr)
 
 (define block
-  (lambda (exprs state)
+  (lambda (exprs state return)
     (cond
       ((null? exprs) (removeSubstate state))
       ((block? exprs) (block (cdr exprs)(addSubstate state) ))
       (else (block (cdr exprs) (statement (car exprs) state))) )))  
 
 (define statement
-  (lambda (expr state)
+  (lambda (expr state return)
     (cond
-      ((eq? (operator expr) 'return) (value (operand1 expr) state))
+      ((eq? (operator expr) 'return) (return (returnHelp expr state)))
       ((and (eq? (operator expr) 'var) (null? (cddr expr))) (declareVariable (operand1 expr) "null" state))
       ((eq? (operator expr) 'var) (declareVariable (operand1 expr) (value (operand2 expr) state) state))
       ((eq? (operator expr) '=) (assignVariable (operand1 expr) (value (operand2 expr) state) state))
-      ((eq? (operator expr) 'if) (ifEval expr state))
-      ((eq? (operator expr) 'while) (whileEval expr state))
+      ((eq? (operator expr) 'if) (ifEval expr state return))
+      ((eq? (operator expr) 'while) (whileEval expr state return))
       )))   
 ; A method to evaluate all of the boolean operations and update their states
 
@@ -94,9 +93,9 @@ Project 1
   (lambda (expr state)
     ;(display (value (cadar expr) state))
     (cond
-      ((eq? (value (cadar expr) state) #t) 'true)
-      ((eq? (value (cadar expr) state) #f) 'false)
-      (else (value (cadar expr) state))
+      ((eq? (value (operand1 expr) state) #t) 'true)
+      ((eq? (value (operand1 expr) state) #f) 'false)
+      (else (value (operand1 expr) state))
       )))
 
 ; A function to evaluate the - symbol works as a negative sign and as an operator
@@ -110,19 +109,31 @@ Project 1
 
 ; A function to evaluate the different possiblities in an if statement or if else statement
 (define ifEval
-  (lambda (expr state)
+  (lambda (expr state return)
     (cond
-      ((and (eq? (operator expr) 'if) (boolean (operand1 expr) state)) (runTree (cons (operand2 expr) '()) state));if succeeds
-      ((not (eq? (operator expr) 'if)) (statement expr state)); else
+      ((and (eq? (operator expr) 'if) (boolean (operand1 expr) state)) (runTree (cons (operand2 expr) '()) state return));if succeeds
+      ((not (eq? (operator expr) 'if)) (runTree (cons expr '()) state return)); else
       ((null? (cdddr expr)) state); last if fails no else
-      (else (ifEval (cadddr expr) state)); else if
+      (else (ifEval (cadddr expr) state return)); else if
     )))
 
 ; A function to evaluate while loops
 
 (define whileEval
-  (lambda (expr state)
+  (lambda (expr state return)
     (cond
-      ((boolean (operand1 expr) state) (whileEval expr (runTree (cons (operand2 expr) '()) state)))
+      ((boolean (operand1 expr) state) (whileEval expr (runTree (cons (operand2 expr) '()) state return) return))
       (else state)
     )))
+
+(define test
+  (lambda (x)
+    (call/cc
+     (lambda (return)
+       (other x return) ))))
+
+(define other
+  (lambda (x return)
+    (cond
+      ((> x 5) (* x (other (- x 1) return)))
+      (else (return x)))))
