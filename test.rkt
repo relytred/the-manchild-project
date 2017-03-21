b11f7a61b9a45788d39f32aac875f5e5e82ff01c
jnoymer <jeffrey.noymer@case.edu>
2017-03-21 19:57:06 -0400

    I have finally added comments to the test file as well as adding our name and other junk at the top

--- test.rkt
+++ test.rkt
@@ -1,18 +1,33 @@
+#|
+Jeffrey Noymer & Tyler Mayle
+jan88 & tcm45
+Project 2
+|#
+
 (load "interpret.rkt")
 (require racket/trace)
+
+
+; A set of expected values testing the first part of the project
 
 (define expectedValues1
   '(150 -4 10 16 220 5 6 10 5 -39 true 100 false true 128 12)
   )
 
+; A set of expected values testing the second part of the project
+
 (define expectedValues2
   '(20 164 32 2 25 21 6 -1 789 12 125 110 2000400 101)
   )
+
+; A function to get the correct test.txt file for test suite 1
 
 (define interpretTest1
   (lambda (i)
     (interpret (string-append "tests/test1/test" (string-append (number->string i) ".txt")))
     ))
+
+; A master function to run the tests for test suite 1
 
 (define runTest1
   (lambda (i values)
@@ -23,10 +38,14 @@
       (else (error "Test" i "should be equal to" (car values) "not" (interpretTest1 i)))
       )))
 
+; A function to get the correct test.txt file for test suite 2
+
 (define interpretTest2
   (lambda (i)
     (interpret (string-append "tests/test2/test" (string-append (number->string i) ".txt")))
     ))
+
+; A master function to run the tests for test suite 2
 
 (define runTest2
   (lambda (i values)
@@ -37,6 +56,9 @@
       (else (error "Test" i "should be equal to" (car values) "not" (interpretTest2 i)))
       )))
 
+; A function to run all of the test suites
+
 (define runTests
-  (print (and (runTest1 1 expectedValues1) (runTest2 1 expectedValues2)))
+  (print (and (runTest1 1 expectedValues1)
+              (runTest2 1 expectedValues2)))
   )
