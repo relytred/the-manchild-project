fb7ec56cabed676b33be9b20d842021bfaefeaea
jnoymer <jeffrey.noymer@case.edu>
2017-04-10 20:09:12 -0400

    I updated the test suite so that all of the values and automatic testing should be operational for the third test suite.

--- test.rkt
+++ test.rkt
@@ -20,6 +20,12 @@ Project 2
   '(20 164 32 2 25 21 6 -1 789 12 125 110 2000400 101)
   )
 
+; A set of expected values testing the third part of the project
+
+(define expectedValues3
+  '(10 14 45 55 1 115 true 20 24 2 35 90 69 87 64 125 100 2000400)
+  )
+
 ; A function to get the correct test.txt file for test suite 1
 
 (define interpretTest1
@@ -56,9 +62,28 @@ Project 2
       (else (error "Test" i "should be equal to" (car values) "not" (interpretTest2 i)))
       )))
 
+; A function to get the correct test.txt file for test suite 3
+
+(define interpretTest3
+  (lambda (i)
+    (interpret (string-append "tests/test3/test" (string-append (number->string i) ".txt")))
+    ))
+
+; A master function to run the tests for test suite 3
+
+(define runTest3
+  (lambda (i values)
+    (cond
+      ((> i 18) #t)
+      ((not (list? values)) "Why")
+      ((eq? (interpretTest3 i) (car values)) (runTest3 (+ i 1) (cdr values)))
+      (else (error "Test" i "should be equal to" (car values) "not" (interpretTest3 i)))
+      )))
+
 ; A function to run all of the test suites
 
 (define runTests
   (print (and (runTest1 1 expectedValues1)
-              (runTest2 1 expectedValues2)))
-  )
+              (runTest2 1 expectedValues2)
+              ;(runTest3 1 expectedValues3)
+              )))
