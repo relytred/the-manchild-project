c287247dc65a35abf292183a7df9a09206cfb93a
jnoymer <jeffrey.noymer@case.edu>
2017-04-30 16:41:30 -0400

    I have updated the test file for the first 13 tests of project 4.

--- test.rkt
+++ test.rkt
@@ -1,7 +1,7 @@
 #|
 Jeffrey Noymer & Tyler Mayle
 jan88 & tcm45
-Project 2
+Project 4
 |#
 
 (load "interpret.rkt")
@@ -24,6 +24,12 @@ Project 2
 
 (define expectedValues3
   '(10 14 45 55 1 115 true 20 24 2 35 90 69 87 64 125 100 2000400)
+  )
+
+; A set of expected values testing the fourth part of the project
+
+(define expectedValues4
+  '(15 12 125 36 54 110 26 117 32 15 123456 5285 -716)
   )
 
 ; A function to get the correct test.txt file for test suite 1
@@ -80,10 +86,29 @@ Project 2
       (else (error "Test" i "should be equal to" (car values) "not" (interpretTest3 i)))
       )))
 
+; A function to get the correct test.txt file for test suite 4
+
+(define interpretTest4
+  (lambda (i)
+    (interpret (string-append "tests/test4/test" (string-append (number->string i) ".txt")))
+    ))
+
+; A master function to run the tests for test suite 4
+
+(define runTest4
+  (lambda (i values)
+    (cond
+      ((> i 13) #t)
+      ((not (list? values)) "Why")
+      ((eq? (interpretTest4 i) (car values)) (runTest4 (+ i 1) (cdr values)))
+      (else (error "Test" i "should be equal to" (car values) "not" (interpretTest4 i)))
+      )))
+
 ; A function to run all of the test suites
 
 (define runTests
   (print (and (runTest1 1 expectedValues1)
               (runTest2 1 expectedValues2)
               ;(runTest3 1 expectedValues3)
+              ;(runTest4 1 expectedValue4)
               )))
